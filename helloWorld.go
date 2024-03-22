9ed8d8b21c844caa825bbae198c9fcd1a9662bcb
Miguel Gomez <miguelalarcon1999@hotmail.com>
2024-03-22 00:59:50 -0500

    hello world

--- helloWorld.go
+++ helloWorld.go
@@ -1,7 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Printf("Hola, Go!")
+
+	// Print hello world with GO
+	fmt.Println("Hola, Go!")
+
+	/*
+		comentario
+			multilinea
+	*/
+
+	// Variales
+	// String
+	var varString string = "Contenido de la variable"
+	fmt.Printf(varString)
+
+	var varString2 = "Esta es una variable sin indicar el tipo"
+	fmt.Println(varString2)
+
+	// Int
+
+	var varInt int = 7
+	varInt = varInt + 4
+	fmt.Println(varInt)
+	fmt.Println(varInt - 1)
+	fmt.Println(varInt)
+
+	// Concatenar texto y numero
+	fmt.Println(varString, varInt)
+
+	// Ver tipo de datos
+	fmt.Println(reflect.TypeOf(varInt))
+
+	// Floar
+	var varFloat float64 = 6.5
+	fmt.Println(varFloat)
+	fmt.Println(reflect.TypeOf(varFloat))
+	//suma de float y enteros
+	fmt.Println(varInt + int(varFloat))
+	fmt.Println(float64(varInt) + varFloat)
+
+	// Boolean
+	var varBool bool = false
+	varBool = true
+	fmt.Println(varBool)
+
+	// Crear variable asignadas automaticamente
+	myVar3 := "Esto es una variable autodeclarada"
+	fmt.Println(myVar3)
+
+	// Constantes
+	const varConstante = "Esto es una constante" //Aúnque una constante no se use, el error permite declaralas y no usarlas
+	fmt.Println(varConstante)
+
+	// Control de flujo (if, else, else if)
+	if varInt == 10 && !varBool {
+		fmt.Println("El valor si es 10")
+	} else if varInt == 12 || varBool {
+		fmt.Println("El valor es 11")
+	} else {
+		fmt.Println("el valor no es 10")
+	}
+
+	//array
+	var myArray [3]int //el numero indica el numero de elemntos
+	myArray[0] = 1
+	myArray[1] = 2
+	myArray[2] = 3
+	fmt.Println(myArray)
+
+	var myArray2 [3]string
+	myArray2[0] = "hola"
+	myArray2[1] = "Mundo"
+	myArray2[2] = "desde Go"
+	fmt.Println(myArray2[2])
+
+	
+
 }
